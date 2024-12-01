b9a3e36f8e762f8dd52a72c182e416baf9d2d28e
MartynaK <martyna.kindrat@gmail.com>
2024-12-01 13:28:42 +0100

    Added music to UI

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -13,16 +13,25 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private float moveDistance = 0.5f;
     [SerializeField] private float speed = 2.0f;
 
+    AudioManager audioManager;
+    
     private Vector3 originalPosition;
     private Vector3 targetPosition;
+
+    private void Awake()
+    {
+        audioManager=GameObject.Find("AudioManager").GetComponent<AudioManager>();
+    }
 
     public void ContinueGame()
     {
         SceneManager.LoadSceneAsync(1);
+        audioManager.PlaySfx(audioManager.menuClick);
     }
     public void NewGame()
     {
         SceneManager.LoadSceneAsync(1);
+        audioManager.PlaySfx(audioManager.menuClick);
     }
     public void ShowSettings()
     {
@@ -33,12 +42,16 @@ public class MainMenu : MonoBehaviour
             settingsPanel.SetActive(false);
             optionButton.GetComponent<Image>().color = new Color(currentColor.r, currentColor.g, currentColor.b, 0);
             targetPosition = eyes.transform.position + new Vector3(0, moveDistance, 0);
+            
+            audioManager.PlaySfx(audioManager.menuClick);
         }
         else
         {
             settingsPanel.SetActive(true);
             optionButton.GetComponent<Image>().color = new Color(currentColor.r, currentColor.g, currentColor.b, 100);
             targetPosition = eyes.transform.position - new Vector3(0, moveDistance, 0);
+            
+            audioManager.PlaySfx(audioManager.menuClick);
         }
 
         StartCoroutine(EyesTransition());
@@ -48,6 +61,7 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+        audioManager.PlaySfx(audioManager.menuClick);
     }
 
     private IEnumerator EyesTransition()
