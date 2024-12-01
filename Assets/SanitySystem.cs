506afd7a5859d563bd7f7da02451184effbd1bcd
WJurgiel <wjurgiel@student.agh.edu.pl>
2024-12-01 10:13:06 +0100

    Sanity system added
    ok

--- Assets/SanitySystem.cs
+++ Assets/SanitySystem.cs
@@ -1,16 +1,68 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SanitySystem : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+   [SerializeField] private float maxSanity;
+   [SerializeField] private float currentSanity;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   [SerializeField] private float baconRegainAmount;
+
+   [SerializeField] private GameObject eyesInTheShadow;
+   [SerializeField] private List<SpriteRenderer> eyesRenderer;
+   
+   // Sanity to zdrowie, odzyskiwanie przy ognisku
+   // sanity wskazywane jest przez to jak wielkie oczy som
+   
+   void Start()
+   {
+      currentSanity = maxSanity;
+      UpdateEyes();
+   }
+
+   void Update()
+   {
+      if (Input.GetKeyDown(KeyCode.J))
+      {
+         RegainSanity();
+      }
+      if (Input.GetKeyDown(KeyCode.K))
+      {
+         LoseSanity(Random.Range(0f, 30f));
+      }
+   }
+   public void RegainSanity()
+   {
+      currentSanity += baconRegainAmount;
+      if(currentSanity >= maxSanity) currentSanity = maxSanity;
+      UpdateEyes();
+   }
+
+   public void FireplaceMeditate()
+   {
+      currentSanity = maxSanity;
+      UpdateEyes();
+   }
+
+   public void LoseSanity(float amount)
+   {
+      currentSanity -= amount;
+      UpdateEyes();
+   }
+   private void UpdateEyes()
+   {
+      if (eyesInTheShadow == null)
+      {
+         Debug.Log("No eyes");
+         return;
+      }
+      if(eyesInTheShadow.activeSelf == false) eyesInTheShadow.SetActive(true);
+      foreach (var eyeElement in eyesRenderer) 
+      {
+         eyeElement.color = new Color(eyeElement.color.r, eyeElement.color.g, eyeElement.color.b,
+            (maxSanity - currentSanity) / maxSanity);
+      }
+     
+   }
 }
